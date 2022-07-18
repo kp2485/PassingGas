99fc6e66fcbc42721ea44e16f11655de503c8ac1
Kyle Peterson <kdpeters@gmail.com>
2022-07-18 17:24:11 -0400

    Initial Commit

---
+++ PassingGas/PassingGasApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassingGasApp.swift
+//  PassingGas
+//
+//  Created by Kyle Peterson on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct PassingGasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
